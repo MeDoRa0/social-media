0dd5011170c9e0817ba5cf418b77d1b4e13754ee
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2024-05-12 22:03:33 +0300

    creat the app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SocialApp());
+}
+
+class SocialApp extends StatelessWidget {
+  const SocialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Placeholder(),
+    );
+  }
+}
