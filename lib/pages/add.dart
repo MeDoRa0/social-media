e6f1feaabad89957a4f53edc358d700bf946a096
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2024-05-14 23:54:52 +0300

    creat add post and image in add page

--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:social_media/app_images.dart';
 import 'package:social_media/colors.dart';
 
 class AddPage extends StatefulWidget {
@@ -11,12 +13,64 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      backgroundColor: kSecondryColor,
-      appBar: AppBar(),
-      body: Center(
-        child: Text('add page'),
+      appBar: AppBar(
+        surfaceTintColor: Colors.white,
+        title: const Text('add post'),
+        actions: [
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.post_add),
+            label: const Text('Post'),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(Assets.imagesMan),
+                ),
+                Gap(30),
+                Expanded(
+                  child: TextField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'type here...'),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: AssetImage(Assets.imagesMan),
+                  ),
+                ),
+              ),
+            ),
+            Gap(30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: kPrimaryColor,
+                padding: EdgeInsets.all(20),
+              ),
+              onPressed: () {},
+              child: Icon(
+                Icons.add_a_photo,
+                color: kWhiteColor,
+              ),
+            ),
+            const Gap(60),
+          ],
+        ),
       ),
     );
   }
