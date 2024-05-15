27c15446f0f9a427a3626ae9f2a2219d37c0bcd4
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2024-05-15 20:15:50 +0300

    creat upload post method

---
+++ lib/services/firestore_cloud.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:social_media/models/post_model.dart';
+import 'package:uuid/uuid.dart';
+
+class CloudMethod {
+  CollectionReference posts = FirebaseFirestore.instance.collection('post');
+
+  uploadPost({
+    required String post,
+    required String userID,
+    required String dispalyName,
+    required String userName,
+    String?
+        profilePicture, //i make it ? nullable , becuase user may not have profile picture
+    required String postImage,
+  }) {
+    String response = 'something error';
+    try {
+      String postID = Uuid().v1();
+      PostModel postModel = PostModel(
+          userID: userID,
+          userName: userName,
+          post: post,
+          postID: postID,
+          postImage: postImage,
+          date: DateTime.now(),
+          like: [],
+          displayName: dispalyName,
+          profilePicture: profilePicture?? '');
+      posts.doc(postID).set(
+            postModel.toJson(),
+          );
+      response = 'post done';
+    } catch (e) {
+      response = e.toString();
+    }
+    return response;
+  }
+}
